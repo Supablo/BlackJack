fbe7b92f3d000a8e52692c6b7b87f1daaed6e389
Supablo <supravos@nycstudents.net>
2024-10-16 21:35:39 -0400

    Part C & D Complete!

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+let firstCard
+let secondCard 
+let sum
+let cards = []
+let hasBlackJack
+let isAlive
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("card-el")
 
-//chalenge #2 
-// code the statements below based on the conditions shown in blackjack.png from the images folder
-// your output should work in the browser's console based on changing the values assigned to the cards
+if (sum < 21) message = "Do you want to draw a new card? 🙂"
+if (sum == 21) message = "Wohoo! You've got Blackjack! 🥳"
+if (sum > 21) message = "You're out of the game! 😭"
+console.log(message) 
 
-//starter code
-message = "Do you want to draw a new card? 🙂"
-message = "Wohoo! You've got Blackjack! 🥳"
-message = "You're out of the game! 😭"
-console.log(message)
+function renderGame()
+{
+    cards[0] = firstCard; cards[1] = secondCard
+    sum = getSum()
+    sumEl.innerText = "Sum: " + sum
+    cardsEl.innerText = "Cards: "
+
+    for (let i = 0; i < cards.length; i++) cardsEl.innerText += " " + cards[i]
+
+    if (sum < 21 && isAlive) messageEl.innerText = "Do you want to draw a new card? 🙂"
+    else if (hasBlackJack) messageEl.innerText = "Wohoo! You've got Blackjack! 🥳"
+    else messageEl.innerText = "You're out of the game! 😭"
+}
+
+function newCard()
+{
+    let card = getRandomCard()
+
+    sum = getSum()
+    cards.push(card)
+
+    renderGame()
+}
+
+function startGame()
+{
+    while (cards[0] != null) cards.pop()
+
+    hasBlackJack = false
+    isAlive = true
+    firstCard = getRandomCard()
+    secondCard = getRandomCard()
+    sum = getSum()
+
+    renderGame()
+}
+
+function getSum()
+{
+    x = 0
+
+    for (let i = 0; i < cards.length; i++) x += cards[i]
+    if (x == 21) hasBlackJack = true
+
+    return x
+}
+
+function getRandomCard()
+{
+    randomNum = Math.floor(Math.random() * 13  + 1)
+    if (randomNum > 9) return 10
+    else if (randomNum == 1) return 11
+    else return randomNum
+}
