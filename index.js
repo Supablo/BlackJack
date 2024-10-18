a43219dee80db6ff8be814e70702e6f89bfd03ab
Supablo <supravos@nycstudents.net>
2024-10-17 20:07:08 -0400

    Part E Complete

--- index.js
+++ index.js
@@ -1,72 +1,93 @@
-let firstCard
-let secondCard 
-let sum
-let cards = []
-let hasBlackJack
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("card-el")
-let blackJackCounter = 0
-let bJ = document.createElement("h3")
+let firstCard;
+let secondCard;
+let sum;
+let cards = [];
+let hasBlackJack;
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("card-el");
+let blackJackCounter = localStorage.getItem("blackJackCounter") ? parseInt(localStorage.getItem("blackJackCounter")) : 0;
+let bJ = document.createElement("h3");
+let pSum = document.createElement("h3");
 
-bJ.innerText = "BlackJack Counter: " + blackJackCounter
-document.body.appendChild(bJ)
+bJ.innerText = "BlackJack Counter: " + blackJackCounter;
+pSum.innerText = "Previous Hand's Sum: " + (localStorage.getItem("previousSum") || 0);
+document.body.appendChild(bJ);
+document.body.appendChild(pSum);
 
-function renderGame()
+function renderGame() 
 {
-    cards[0] = firstCard; cards[1] = secondCard
-    sum = getSum()
-    sumEl.innerText = "Sum: " + sum
-    cardsEl.innerText = "Cards: "
+    cards[0] = firstCard;
+    cards[1] = secondCard;
+    sum = getSum();
+    sumEl.innerText = "Sum: " + sum;
+    cardsEl.innerText = "Cards: ";
 
-    for (let i = 0; i < cards.length; i++) cardsEl.innerText += " " + cards[i]
+    for (let i = 0; i < cards.length; i++) cardsEl.innerText += " " + cards[i];
 
-    if (sum < 21) messageEl.innerText = "Do you want to draw a new card? 🙂"
-    else if (hasBlackJack) messageEl.innerText = "Wohoo! You've got Blackjack! 🥳"
-    else messageEl.innerText = "You're out of the game! 😭"
+    if (sum < 21) messageEl.innerText = "Do you want to draw a new card? 🙂";
+    else if (hasBlackJack) messageEl.innerText = "Wohoo! You've got Blackjack! 🥳";
+    else messageEl.innerText = "You're out of the game! 😭";
+
+    localStorage.setItem("cards", JSON.stringify(cards));
+    localStorage.setItem("sum", sum);
+    localStorage.setItem("hasBlackJack", hasBlackJack);
 }
 
-function newCard()
+function newCard() 
 {
-    let card = getRandomCard()
-
-    sum = getSum()
-    cards.push(card)
-
-    renderGame()
+    let card = getRandomCard();
+    cards.push(card);
+    renderGame();
 }
 
-function startGame()
+function startGame() 
 {
-    while (cards[0] != null) cards.pop()
+    while (cards.length > 0) cards.pop();
 
-    hasBlackJack = false
-    firstCard = getRandomCard()
-    secondCard = getRandomCard()
-    sum = getSum()
+    pSum.innerText = "Previous Hand's Sum: " + sum;
+    localStorage.setItem("previousSum", sum);
 
-    renderGame()
+    hasBlackJack = false;
+    firstCard = getRandomCard();
+    secondCard = getRandomCard();
+    sum = getSum();
+
+    renderGame();
 }
 
-function getSum()
+function getSum() 
 {
-    x = 0
+    let x = 0;
 
-    for (let i = 0; i < cards.length; i++) x += cards[i]
+    for (let i = 0; i < cards.length; i++) x += cards[i];
     if (x == 21) 
     {
-        hasBlackJack = true
-        blackJackCounter++
-        bJ.innerText = "BlackJack Counter: " + blackJackCounter
+        hasBlackJack = true;
+        blackJackCounter++;
+        bJ.innerText = "BlackJack Counter: " + blackJackCounter;
+        localStorage.setItem("blackJackCounter", blackJackCounter);
     }
 
-    return x
+    return x;
 }
 
-function getRandomCard()
+function getRandomCard() 
 {
-    randomNum = Math.floor(Math.random() * 13  + 1)
-    if (randomNum > 9) return 10
-    else if (randomNum == 1) return 11
-    else return randomNum
+    let randomNum = Math.floor(Math.random() * 13 + 1);
+
+    if (randomNum > 9) return 10;
+    else if (randomNum == 1) return 11;
+    else return randomNum;
+}
+
+window.onload = function() 
+{
+    if (localStorage.getItem("cards")) 
+    {
+        cards = JSON.parse(localStorage.getItem("cards"));
+        sum = parseInt(localStorage.getItem("sum"));
+        hasBlackJack = localStorage.getItem("hasBlackJack") === "true";
+        renderGame();
+    }
 }
